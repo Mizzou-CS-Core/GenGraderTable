73dc787e6bd2ace0606afefe79a35aab49eca28c
Matt Marlow <thequantumblink@gmail.com>
2025-04-28 16:35:36 -0500

    feat: modernized for pip 23.1+

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name="gen_grader_table",
   version="0.2",
   packages=find_packages(include=["gen_grader_table", "gen_grader_table.*"]),
-    install_requires=[
-        "canvas_lms_api",
-    ],
+  install_requires=[
+  "canvas_lms_api @ git+https://github.com/Mizzou-CS-Core/CanvasRequestLibrary.git#egg=canvas_lms_api",
+  ],
 )
