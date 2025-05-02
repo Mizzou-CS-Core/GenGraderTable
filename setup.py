a794e371212a8cf32f0773ff67a43221a36970e8
Matt Marlow <thequantumblink@gmail.com>
2025-05-02 16:00:47 -0500

    feat: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gen_grader_table",
-    version="0.2",
+    version="0.3",
     packages=find_packages(include=["gen_grader_table", "gen_grader_table.*"]),
     install_requires=[
         "canvas_lms_api @ git+https://github.com/Mizzou-CS-Core/CanvasRequestLibrary.git#egg=canvas_lms_api",
